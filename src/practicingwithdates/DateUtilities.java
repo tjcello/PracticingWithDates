6180f1e1531571cfe6df322a9077ed7122c326ae
Thomas <Thomas@10.30.18.233>
2015-10-26 15:26:04 -0500

    added 2 toDate functions and 2 toString options

--- src/practicingwithdates/DateUtilities.java
+++ src/practicingwithdates/DateUtilities.java
@@ -1,16 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicingwithdates;
 
+import java.text.DateFormat;
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.TemporalField;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import javax.swing.text.DateFormatter;
 
 /**
  *
@@ -18,18 +24,75 @@ import java.time.temporal.TemporalField;
  */
 public class DateUtilities {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.now();
-        System.out.println(date);
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd  hh:mm a");
-
-        ZonedDateTime local = ZonedDateTime.now();
-        local = local.withZoneSameInstant(ZoneId.of("Europe/Rome"));
-
-        System.out.println(local.format(fmt));
+//    public LocalDateTime getCurrentDateTime(){
+//        return LocalDateTime.now();
+//    }
+    
+    public Duration getDateDiff(){
+        LocalDateTime startDate = LocalDateTime.now();
+        LocalDateTime endDate = startDate.withDayOfMonth(22).withDayOfYear(360);
+        Duration diff = Duration.between(startDate, endDate);
+        return diff;
+       
     }
+    
+    public LocalDateTime getFiveDaysFromNowTwoHoursAgo(){
+       LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dueDate = now.plusDays(5).minusHours(2);
+        return dueDate;
+    }
+    
+   public String toString(LocalDateTime date){
+       DateTimeFormatter df = DateTimeFormatter.ISO_DATE_TIME;
+       return df.format(date);   
+     
+   }
+   
+   public String toString(LocalDate d){
+       DateTimeFormatter formats = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+       return formats.format(d);
+   }
+    
+   public LocalDate toDate(String it){
+       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	LocalDate mine = LocalDate.parse(it, formatter);
+        return mine;	
+	
+   }
+   
+   public LocalDateTime toDateTime(String time){
+       DateTimeFormatter formats = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"); 
+       LocalDateTime ours= LocalDateTime.parse(time,formats);
+       return ours;
+       
+   }
+    
+    public static void main(String[] args) {
+        DateUtilities app = new DateUtilities();
+//        LocalDateTime date = app.getCurrentDateTime();
+//        System.out.println(date);
+        
+        Duration diff = app.getDateDiff();
+        System.out.println(diff.toMinutes() + " minutes to go til date entered");
+        System.out.println(diff.toDays()+ " days til the date entered");
+        System.out.println(" ");
+        
+        LocalDateTime dueDate = app.getFiveDaysFromNowTwoHoursAgo();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM.dd.yyyy  hh:mm a");
+        System.out.println(dueDate.format(fmt) + " is exactly 5 days ahead/ 2 hours behind");
+        
+        
+        System.out.println(" ");
+        
+           LocalDate date = app.toDate("1985-10-18");
+           System.out.println(date.format(DateTimeFormatter.ofPattern("MM-dd-yyyy")));
+           LocalDateTime date2 = app.toDateTime("1985-10-18 08:22");
+           System.out.println(date2.format(DateTimeFormatter.ofPattern("mm:HH dd-yyyy-MM")));
+        
+        
+    }
+   
 
+    
+    
 }
